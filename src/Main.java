68fa59ff4efd2023e931594dcde7cc7ba1a70ee9
Dmitry Hotko <d.hotko@invento.by>
2022-06-03 15:37:54 +0300

    test

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+
 public class Main {
     public static void main(String[] args) {
         Twit system = new Twit();
@@ -27,7 +28,6 @@ public class Main {
         dima.add(".!.", "Fuck you!");
         dima.like(".!.");
         stas.like("Хаха. Расчет дураков окончен!");
-
         system.board();
 
     }
